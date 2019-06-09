2d4a291e8f667f86c7d2c4cde8248634f74a775f
ahmedfrontend <ahmedfrontend@gmail.com>
2019-06-10 00:22:02 +0200

    .gitignore fix

---
+++ WindowsServicesConsoleApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace WindowsServicesConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var exitCode = HostFactory.Run(x =>
+            {
+                x.Service<ServicesClass>(s =>
+                {
+                    s.ConstructUsing(servicesClass => new ServicesClass());
+                    s.WhenStarted(servicesClass => servicesClass.Start());
+                    s.WhenStopped(servicesClass => servicesClass.Stop());
+
+                });
+                x.SetServiceName("WindowsSerivceConsole");
+                x.SetDisplayName("Windows Serivce Console");
+                x.SetDescription("this is sample windows services App!!!");
+
+
+            });
+            int exitCodeValue = (int)Convert.ChangeType(exitCode, exitCode.GetTypeCode());
+            Environment.ExitCode = exitCodeValue;
+        }
+    }
+}
---
+++ WindowsServicesConsoleApp/ServicesClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace WindowsServicesConsoleApp
+{
+  public  class ServicesClass
+    {
+        private readonly Timer _timer;
+
+        public ServicesClass()
+        {
+            _timer = new Timer(1000) { AutoReset = true };
+            _timer.Elapsed += _timer_Elapsed;
+        }
+
+        private void _timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            string[] liens = new string[] { DateTime.Now.ToString() };
+            File.AppendAllLines(@"D:\AA\TestApps\WindowsservicesFiles\TimeSheet.txt", liens);
+
+        }
+        public void Start() {
+            _timer.Start();
+        }
+        public void Stop() {
+            _timer.Stop();
+        }
+    }
+}
